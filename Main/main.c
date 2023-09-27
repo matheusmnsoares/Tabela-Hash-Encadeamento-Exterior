ea12adf300cd00c267b7c081b15594176c4a4ae4
Jansen-Alves <75702771+Jansen-Alves@users.noreply.github.com>
2023-09-26 21:07:45 -0300

    Update main.c

--- Main/main.c
+++ Main/main.c
@@ -4,24 +4,46 @@
 #include "compartimento_hash.h"
 
 int main(){
-    
-    menu();
-
-    return 0;
-}
-
-void menu(){
-    int escolha;
     FILE *hash;
     FILE *meta;
     FILE *clientes;
 
+
+    if ((hash = fopen("tabHash.dat", "r+b")) == NULL){
+    printf("Erro ao abrir o arquivo da tabela Hash");
+    exit(1);
+    }
+    if ((meta = fopen("meta.dat", "r+b")) == NULL){
+    printf("Erro ao abrir o arquivo da tabela Meta");
+    exit(1);
+    }
+
+    if ((hash = fopen("clientes.dat", "r+b")) == NULL){
+    printf("Erro ao abrir o arquivo da tabela Clientess");
+    exit(1);
+    }
+    
+    menu(hash, meta, clientes);
+
+    return 0;
+}
+
+void menu(FILE *hash, FILE *meta, FILE *clientes){
+    int escolha, chave;
+    char nome[100];
+    Cliente *novo;
+    
 
     printf("Menu:\n 1 - Inserir \n 2 - Deletar \n 3 - Busca \n 4 - Zerar \n 5 - Sair \n");
     scanf("%d", &escolha);
     switch (escolha)
     {
     case 1:
+        printf("Digite o número da chave do cliente: \n");
+        scanf("%d", &chave);
+        printf("Digite o nome do cliente:\n");
+        scanf("%s", nome);
+        novo = criarCliente(chave, nome);
         inserir();
         menu();
         break;
